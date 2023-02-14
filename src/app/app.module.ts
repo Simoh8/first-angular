0dda1e3e55f613510f50e0f1736e4757a1ec708d
simon muturi <simomutu@gmail.com>
2023-02-14 22:26:19 +0300

    hello angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Header } from 'src/Header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+Header
+
   ],
   imports: [
     BrowserModule,
